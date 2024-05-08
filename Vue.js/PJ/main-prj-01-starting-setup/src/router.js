8068c99bc39cff52cd649f0c56d701cc2cbdf8d9
Kang Duoh <kduoh99@gmail.com>
2024-05-09 01:08:48 +0900

    feat: 코치 세부 사항 컴포넌트 구축

--- Vue.js/PJ/main-prj-01-starting-setup/src/router.js
+++ Vue.js/PJ/main-prj-01-starting-setup/src/router.js
@@ -10,15 +10,18 @@ import NotFound from './pages/NotFound.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/coaches'},
+    { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
-    { path: '/coaches/:id', component: CoachDetail, children: [
-        { path: 'contact', component: ContactCoach },
-      ] },
+    {
+      path: '/coaches/:id', component: CoachDetail, props: true,
+      children: [
+        { path: 'contact', component: ContactCoach }
+      ]
+    },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
+    { path: '/:notFound(.*)', component: NotFound }
+  ]
 });
 
 export default router;

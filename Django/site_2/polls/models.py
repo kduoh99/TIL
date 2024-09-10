8ec46e40792f9fc7b9c4a7bbeb2e1c7a4f7d1a25
Kang Duoh <kduoh99@gmail.com>
2024-09-10 11:26:40 +0900

    feat: 실시간 설문조사 구현

---
+++ Django/site_2/polls/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class Question(models.Model): # 설문조사 주제 table
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+
+    was_published_recently.boolean = True
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model): # 설문조사 선택지 table
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
